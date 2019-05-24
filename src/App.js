a1f0a40191740c7842b6f84dbe5a2cb5f490fbef
dinesh shrestha <dinu.shrestha777@gmail.com>
2019-05-24 12:20:12 +0200

    installed bootstrap and react-dom

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import {BrowserRouter as Router, Route, Link } from  "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        <h2>Certificate checker</h2>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
